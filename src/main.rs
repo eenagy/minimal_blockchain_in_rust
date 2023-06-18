399623a29a1ece5a3c8fa85b5f0ae2a097f2121f
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2023-06-18 12:09:20 +0700

    chore: empty main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,1 @@
-mod blockchain;
-
-fn main() {
-    let mut chain = blockchain::Blockchain::new();
-
-    // Add some blocks to the chain
-    chain.add_block("Block 1".to_string());
-    chain.add_block("Block 2".to_string());
-    chain.add_block("Block 3".to_string());
-
-    // Print the blockchain
-    println!("{:#?}", chain);
-}
+fn main() {}
